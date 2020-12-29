cb4b66870f3e43f04ce400290dae46eab9de04eb
Daniel Hartley <danhartleybcn@gmail.com>
2020-12-29 08:25:01 +0000

    handle providers without lessons

--- pages/providers/index.tsx
+++ pages/providers/index.tsx
@@ -1,15 +1,18 @@
 import Link from 'next/link';
 import Layout from 'components/layout/layout';
 import providers from 'pages/providers/providers.json';
+import { getLessons } from 'api/lessons/utils';
 
-const Providers = ({providers}) => {
+const Providers = ({providers, lessonsProviders}) => {
 
-const _providers = providers.map(provider => <li><Link href={`/providers/${provider.slug}/lessons`}><a>{provider.name}</a></Link></li>);
+
+const providersWithLessons = providers.filter(p => lessonsProviders.indexOf(p.slug) > -1);
+const providerList = providersWithLessons.map(provider => <li><Link href={`/providers/${provider.slug}/lessons`}><a>{provider.name}</a></Link></li>);
 
     return (
         <Layout title="Providers" header="Providers">
             <ul>
-                { _providers }
+                { providerList }
             </ul>
         </Layout>
     )
@@ -17,10 +20,12 @@ const _providers = providers.map(provider => <li><Link href={`/providers/${provi
 
 export default Providers;
 
-export async function getStaticProps() {  
+export async function getStaticProps() {
+    const lessonsProviders = getLessons().map(l => l.provider);
     return {
       props: {
         providers: providers.providersList,
+        lessonsProviders
       },
     }
 }
