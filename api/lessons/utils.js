9d761a92b5e009b7c768276e8865dc7a7d45d4c0
Daniel Hartley <danhartleybcn@gmail.com>
2021-01-11 15:26:55 +0000

    hide test and template lessons from live

--- api/lessons/utils.js
+++ api/lessons/utils.js
@@ -7,7 +7,11 @@ export const getLessons = () => {
     
     const lessons = [];
 
-    const files = fs.readdirSync('api/lessons').filter(file => path.extname(file).toLowerCase() === EXTENSION);
+    let files = fs.readdirSync('api/lessons').filter(file => path.extname(file).toLowerCase() === EXTENSION);
+
+    console.log(files);
+
+    files = files.filter(file => file.indexOf('_ignore_') === -1);
     
     files.forEach(file => {
         const lesson = JSON.parse(fs.readFileSync(`api/lessons/${file}`, 'utf8'));
